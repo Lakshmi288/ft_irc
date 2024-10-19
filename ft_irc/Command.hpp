bbdb180257b8f2184f1a43fd886abe6f65cef33d
Lakshmi-Saseendran <94198472+Lakshmi288@users.noreply.github.com>
2024-10-19 13:16:55 +0400

    Add files via upload

---
+++ ft_irc/Command.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Command.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lps <lps@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 11:09:41 by lps               #+#    #+#             */
+/*   Updated: 2024/10/11 11:09:42 by lps              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <string>
+#include "Server.hpp"
+#include "Channel.hpp"
+
+#define VALID_LEN 15
+class Server;
+
+struct msg_struct
+{
+	std::string prefix;
+	std::string cmd;
+	std::string parameter;
+};
+
+int  pass(Server *server, int clifdent_fd, msg_struct msg_info);
+void nick(Server *server, int fd, msg_struct msg_info);
+void user(Server *server, int client_fd, msg_struct msg_info);
+void quit(Server *server, int fd, msg_struct msg_info);
+void pong(Server *server, int client_fd, msg_struct msg_info);
+int  join(Server *server, int client_fd, msg_struct msg_info);
+void kick(Server *server, int client_fd, msg_struct msg_info);
+int  part(Server *server, int client_fd, msg_struct msg_info);
+int  invite(Server *server, int client_fd, msg_struct msg_info);
+int  topic(Server *server, int client_fd, msg_struct msg_info);
+void  list(Server *server, int fd, msg_struct cmd_infos);
+
+#endif
